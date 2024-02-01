5a9ee8af6206014dbd9f8573b8d5e466cac9bde2
NoUCorn <76876304+NoUCorn@users.noreply.github.com>
2024-02-01 16:24:37 +0100

    rubber ball test

--- Simulations/RigidBodySystemSimulator.cpp
+++ Simulations/RigidBodySystemSimulator.cpp
@@ -402,6 +402,37 @@ void RigidBodySystemSimulator::setupAngryBirdsDemo() {
 	structure.addSpring(0, 3, Vec3(0, 0, +0.27), Vec3(0, 0.5, 0), initialLength, 40);
 	
 	m_vSpringStructures.push_back(structure);
+
+
+
+	// Daniel added: rubber ball
+	structure = SpringStructure();
+	structure.setExternalForce(Vec3(0, -m_fGravity, 0));
+
+	float circleSize = 1.5f;
+	int circlePartsAmount = 7;
+	for (int i = 0; i < circlePartsAmount; i++) {
+		float curX = cos(2 * M_PI * i / (float)circlePartsAmount) * circleSize;
+		float curY = sin(2 * M_PI * i / (float)circlePartsAmount) * circleSize;
+
+		// corners (RBs)
+		Rigidbody* circlePart = new Rigidbody("RubberBallPart_" + to_string(i), &m_SimulationParameters,
+			1, // mass
+			Vec3(curX, curY + 3, 0),  // pos   // the +3 is just for some initial falling height. can be adjusted
+			Vec3(0, 0, 0),  // rot
+			Vec3(0.5, 0.5, 0.5));  // scale
+		circlePart->color = Vec3(0.6, 0.27, 0.03);
+
+		structure.addRigidbody(circlePart);
+		//circlePart->setForce(Vec3(0, -m_fGravity, 0));
+		//circlePart.setLinearVelocity(Vec3(0, -0.2, 0));
+		//m_vRigidbodies.push_back(circlePart);
+		//std::cout << "rb mit" << curX << endl;
+
+		// springs
+
+
+	}
 }
 
 /* OLD VERSION :
